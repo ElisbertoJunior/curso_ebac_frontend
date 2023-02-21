413d8a5ca45c12b6723c989821d59b31cfde2ffc
Elisberto Junior <elisberto_jr@hotmail.com>
2023-02-21 14:38:22 -0300

    adicionando calculadora-de-notas

---
+++ calculadora-de-medias/main.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form-atividade');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const inputNomeAtividade = document.getElementById("form-atividade");
+  const inputNotaAtividade = document.getElementById("nota-atividade");
+});
