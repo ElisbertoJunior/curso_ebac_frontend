236d8c07bfa415df7295b24086f073ce8dae169c
Elisberto Junior <elisberto_jr@hotmail.com>
2023-02-21 21:00:55 -0300

    finalizando calculadora-de-notas

--- calculadora-de-medias/main.js
+++ calculadora-de-medias/main.js
@@ -1,8 +1,77 @@
-const form = document.getElementById('form-atividade');
+const form = document.getElementById("form-atividade");
+const imgApproved = `<img src="./images/aprovado.png">`;
+const imgDisapproved = `<img src="./images/reprovado.png">`;
+const activities = [];
+const grades = [];
+const spanApproved = `<span class="result approved">Aprovado</span>`;
+const spanDisapproved = `<span class="result disapproved">Reprovado</span>`;
 
-form.addEventListener('submit', (event) => {
+let row;
+
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const inputNomeAtividade = document.getElementById("form-atividade");
-  const inputNotaAtividade = document.getElementById("nota-atividade");
+  addRow();
+  updateTable();
+  upateFinalMedia();
+
 });
+
+
+const addRow = () => {
+  const inputNomeAtividade = document.getElementById("nome-atividade");
+  const inputNotaAtividade = document.getElementById("nota-atividade");
+
+  
+  if (activities.includes(inputNomeAtividade.value)) { 
+    alert(`A atividadde - ${inputNomeAtividade.value} ja existe.`); 
+    row = '';
+    return
+  } 
+
+  activities.push(inputNomeAtividade.value);
+  grades.push(parseFloat(inputNotaAtividade.value));
+
+  row = `<tr>`;
+  row += `<td>${inputNomeAtividade.value}</td>`;
+  row += `<td>${inputNotaAtividade.value}</td>`;
+  row += `<td>${
+    inputNotaAtividade.value >= 7 ? imgApproved : imgDisapproved
+  }</td>`;
+  row += `</tr>`;
+
+  inputNomeAtividade.value = "";
+  inputNotaAtividade.value = "";
+
+  
+};
+
+const updateTable = () => {
+  const tableBody = document.querySelector("table");
+  tableBody.innerHTML += row;
+};
+
+const calcfinalMedia = () => {
+ const soma = grades.reduce((acc, curretValue) => acc + curretValue);
+ const media = soma / grades.length;
+ return media.toFixed(1);
+  
+}
+  
+
+const upateFinalMedia = () => {
+  const tableFooter = document.querySelector("tfoot");
+  const finalMedia = calcfinalMedia();
+   
+  const tFootRow = `
+      <tr>
+        <td>Media final</td>
+        <td>${finalMedia}</td>
+        <td>${finalMedia >= 7 ? spanApproved : spanDisapproved}</td>
+      </tr>
+  `;
+
+  tableFooter.innerHTML = tFootRow;
+};
+
+
