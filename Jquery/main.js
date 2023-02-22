4e5522ea4fca03cfe5c3842a0d7628a0c9803f74
Elisberto Junior <elisberto_jr@hotmail.com>
2023-02-22 13:15:42 -0300

    finalizando jquery

--- Jquery/main.js
+++ Jquery/main.js
@@ -1,9 +1,27 @@
 $(document).ready(() => {
-  $('header button').click(() => {
-    $('form').slideDown();
+  $("header button").click(() => {
+    $("form").slideDown();
   });
 
-  $('#button-cancel').click(() => {
-    $('form').slideUp()
-  })
+  $("#button-cancel").click(() => {
+    $("form").slideUp();
+  });
+
+  $("form").on("submit", (event) => {
+    event.preventDefault();
+    const newImageAddress = $('#image-address').val();
+    const newItem = $(`<li style="display: none"></li>`);
+    $(`<img src="${newImageAddress}" />`).appendTo(newItem);
+    $(`
+        <div class="overlay-image-link">
+          <a href="${newImageAddress}" target="_blank" title="Ver imagem em tamanho real"/>
+            Ver imagem em tamanho real
+          </a>
+        </div>
+      `).appendTo(newItem);
+
+      $(newItem).appendTo('ul');
+      $(newItem).fadeIn(1000);
+      $('#image-address').val('');
+  });
 });
