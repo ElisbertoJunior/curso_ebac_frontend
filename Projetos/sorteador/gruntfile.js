63b92a1a3184ab74056d682594ce92ff9fd755f5
Elisberto Junior <elisberto_jr@hotmail.com>
2023-03-09 22:05:50 -0300

    finalizando grunt

--- Projetos/sorteador/gruntfile.js
+++ Projetos/sorteador/gruntfile.js
@@ -33,7 +33,11 @@ module.exports = function (grunt) {
             {
               match: 'ENDERECO_DO_CSS', 
               replacement: './styles/main.css'
-            }
+            },
+            {
+              match: 'ENDERECO_DO_JS', 
+              replacement: '../src/scripts/main.js'
+            },
           ]
         },
         files: [
@@ -51,7 +55,11 @@ module.exports = function (grunt) {
             {
               match: 'ENDERECO_DO_CSS', 
               replacement: './styles/main.min.css'
-            }
+            },
+            {
+              match: 'ENDERECO_DO_JS', 
+              replacement: './scripts/main.min.js'
+            },
           ]
         },
         files: [
@@ -75,8 +83,14 @@ module.exports = function (grunt) {
         }
       }
     },
-    clean: ["prebuild"]
-    
+    clean: ["prebuild"],
+    uglify: {
+      target: {
+        files: {
+          'dist/scripts/main.min.js' : 'src/scripts/main.js'
+        }
+      }
+    }
   });
 
  
@@ -85,9 +99,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-replace");
   grunt.loadNpmTasks("grunt-contrib-htmlmin");
   grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
  
 
   grunt.registerTask("default", ["watch"]);
-  grunt.registerTask("build", ["less:production", "htmlmin:dist", "replace:dist", "clean"]);
+  grunt.registerTask("build", ["less:production", "htmlmin:dist", "replace:dist", "clean", "uglify"]);
 
 };
