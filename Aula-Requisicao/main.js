190d92f9fee15c049414af8b9faf52f7558a6256
Elisberto Junior <elisberto_jr@hotmail.com>
2023-03-23 23:32:31 -0300

    iniciando ajax

--- Aula-Requisicao/main.js
+++ Aula-Requisicao/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById('btn-search-cep')
     .addEventListener('click', () => {
@@ -9,11 +10,20 @@ document.addEventListener("DOMContentLoaded", () => {
       xhttp.send();
     })
 });
+*/
 
-$(document).ready(() => {
-  $('btn-search-cep').click(() => {
-    const cep = $('cep').val()
-    const endpoint = `https://viacep.com.br/ws/${cep}/json`
-    
-  })
-})
+$(document).ready(function () {
+  $("#btn-search-cep").click(function () {
+    const cep = $("#cep").val();
+    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+    $(this).find()
+
+    $.ajax(endpoint).done(function (res) {
+      const { logradouro, bairro, localidade, uf } = res;
+
+      const address = `${logradouro}, ${bairro}, ${localidade}, ${uf}`;
+
+      $("#address").val(address);
+    });
+  });
+});
