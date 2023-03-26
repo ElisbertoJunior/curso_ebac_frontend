9eb5e00e3c2bb19a0780cb306290cfb177d28e1f
Elisberto Junior <elisberto_jr@hotmail.com>
2023-03-26 13:30:25 -0300

    iniciando POO

--- orientacao-obj/heranca.js
+++ orientacao-obj/heranca.js
@@ -7,8 +7,22 @@ function Pessoa(nome) {
 }
 
 function Funcionario(nome, cargo, salario) {
-  this.cargo = cargo,
-  this.salario = salario
+  this.cargo = cargo;
+  // this.salario = salario
+  let _salario = salario;
+
+  //getters e setters
+  this.getSalario = function() {
+    return _salario
+  }
+
+  this.setSalario = function(valor) {
+    if(typeof valor === 'number') {
+      _salario = valor;
+    }
+    
+  }
+
   this.dizCargo = function() {
     console.log(this.cargo)
   }
@@ -20,3 +34,6 @@ const pessoa1 = new Pessoa("Maria");
 const funcionario1 = new Funcionario("Maria", "Dev front-end", 4000);
 funcionario1.dizOi();
 funcionario1.dizCargo();
+funcionario1.setSalario(6000);
+
+console.log(funcionario1.getSalario());
