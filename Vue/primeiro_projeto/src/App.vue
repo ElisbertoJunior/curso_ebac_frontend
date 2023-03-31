4fc3d5b69e6ee113a7f60eb6c6ba90a2febc7b14
Elisberto Junior <elisberto_jr@hotmail.com>
2023-03-30 21:02:38 -0300

    iniciando no vue

---
+++ Vue/primeiro_projeto/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  const nome = 'Junior';
+
+  const dizOi = (name) => {
+    return `${name} disse Oi.`
+  }
+
+  const imgBatman = `https://files.tecnoblog.net/wp-content/uploads/2021/04/Qual-a-ordem-cronologica-dos-filmes-do-Batman-Deny-Freeman-Flickr.jpg`;
+
+  const btnDisabled = false;
+
+</script>
+
+<template>
+  <h1>{{ dizOi('Junior') }}</h1>
+  <img v-bind:src="imgBatman" alt="Batman">
+
+  <button :disabled="!btnDisabled">Enviar</button>
+</template>
+
+<style scoped>
+
+</style>
